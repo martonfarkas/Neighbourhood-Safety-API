f1045fd55a80f4e90021758d031e590b6edd94bf
martonfarkas <14434@coderacademy.edu.au>
2023-06-24 11:31:13 +1000

    added init file and imports installed

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
+
 
 @app.route('/')
 def index():
     return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
