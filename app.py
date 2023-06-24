c1b6cdbd3afc32da5d89394cad69f3f62a267491
martonfarkas <14434@coderacademy.edu.au>
2023-06-24 20:48:49 +1000

    get one alert

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from os import environ
 from init import db, ma, bcrypt, jwt
 from blueprints.cli_bp import cli_bp
 from blueprints.auth_bp import auth_bp
+from blueprints.alert_bp import alert_bp
+from blueprints.incident_bp import incident_bp
+from blueprints.location_bp import location_bp
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,5 +19,9 @@ ma.init_app(app)
 jwt.init_app(app)
 bcrypt.init_app(app)
 
-app.register_blueprint(cli_bp)
-app.register_blueprint(auth_bp)
+app.register_blueprint(cli_bp, name='cli_bp')
+app.register_blueprint(auth_bp, name='auth_bp')
+app.register_blueprint(alert_bp, name='alert_bp')
+app.register_blueprint(incident_bp, name='incident_bp')
+app.register_blueprint(location_bp, name='location_bp')
+
--- blueprints/alert_bp.py
+++ blueprints/alert_bp.py
@@ -10,3 +10,12 @@ def get_alerts():
     stmt = db.select(Alert).order_by(Alert.alert_id.desc())
     alerts = db.session.scalars(stmt).all()
     return AlertSchema(many=True).dump(alerts)
+
+@alert_bp.route('/<int:alert_id>', methods=['GET'])
+def one_alert(alert_id):
+    stmt = db.select(Alert).filter_by(id=alert_id)
+    alert = db.session.scalar(stmt)
+    if alert:
+        return AlertSchema().dump(alert)
+    else:
+        return {'error': 'Alert not found'}, 404
