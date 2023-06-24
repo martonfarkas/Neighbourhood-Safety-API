fdfbe1ec244de470ffeeb6578967507bd251cc20
martonfarkas <14434@coderacademy.edu.au>
2023-06-24 21:08:47 +1000

    get one location

--- blueprints/location_bp.py
+++ blueprints/location_bp.py
@@ -9,3 +9,12 @@ def get_locations():
     stmt = db.select(Location).order_by(Location.city.desc())
     locations = db.session.scalars(stmt).all()
     return LocationSchema(many=True).dump(locations)
+
+@location_bp.route('/<int:location_id>', methods=['GET'])
+def one_location(location_id):
+    stmt = db.select(Location).filter_by(id=location_id)
+    location = db.session.scalar(stmt)
+    if location:
+        return LocationSchema().dump(location)
+    else:
+        return {'error': 'Location not found'}, 404
